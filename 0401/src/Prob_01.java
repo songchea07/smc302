e9440da9fe3a56cd082ab7ff7c02ec107088f3fa
song <thdcodus1421@gmail.com>
2021-04-01 15:33:29 +0900

    0401 프로젝트 생성

---
+++ 0401/src/Prob_01.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Prob_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		Grade me=new Grade();
+		
+		System.out.print("수학 과학 영어 순으로 3개의 점수 입력 >>");
+		me.math = scan.nextInt();
+		me.science = scan.nextInt();
+		me.english = scan.nextInt();
+		
+		System.out.println("평균은 "+me.average());  
+		
+		
+	
+		
+	}
+
+}
+
+class Grade{
+	int math;
+	int science;
+	int english;
+	
+	public int average(){
+		return (math+science+english)/3;
+
+		
+		
+	}
+}
